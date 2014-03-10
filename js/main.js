307a34d62517bca66d9a3384beff86e0942fa456
dylanhochreiter <dyl_hoch@live.com>
2014-03-10 13:00:33 -0400

    Changes to layout
    Other HTML added. Google Maps integrated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
+function maps(){
 
+	var map;
+	var brooklyn = new google.maps.LatLng(45.42963, -75.68841);
+
+	var MY_MAPTYPE_ID = 'custom_style';
+
+	function initialize() {
+
+	  var featureOpts = [
+	    {
+	      stylers: [
+	        { hue: '#ffa500' },
+	        { visibility: 'simplified' },
+	        { gamma: 0.5 },
+	        { weight: 1.0 }
+	      ]
+	    },
+	    {
+	      featureType: 'water',
+	      stylers: [
+	        { color: '#293542' }
+	      ]
+	    }
+	  ];
+
+	  var mapOptions = {
+	    zoom: 8,
+	    center: brooklyn,
+	    mapTypeControlOptions: {
+	      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+	    },
+	    mapTypeId: MY_MAPTYPE_ID
+	  };
+
+	  map = new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+
+	  var styledMapOptions = {
+	    name: 'Custom Style'
+	  };
+
+	  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+	  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+}
+
+maps();
